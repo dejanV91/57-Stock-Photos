c6d775a88cd2487903020dad8b745d921edb1a19
dejanV91 <dejanvukoicic91@gmail.com>
2022-12-12 14:03:16 +0100

    display images

--- src/App.js
+++ src/App.js
@@ -3,29 +3,56 @@ import { FaSearch } from "react-icons/fa";
 import Photo from "./Photo";
 // const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 // key : l0s9AheB325heFe16vleHxLkfrDd5JRn1Eis3CABSBM;
+const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`;
 const mainUrl = `https://api.unsplash.com/photos/`;
 const searchUrl = `https://api.unsplash.com/search/photos/`;
 
 function App() {
   const [loading, setLoading] = useState(false);
   const [photos, setPhotos] = useState([]);
-
   const fetchImages = async () => {
-    let url = `${mainUrl}?client_id=l0s9AheB325heFe16vleHxLkfrDd5JRn1Eis3CABSBM`;
+    let url = `${mainUrl}${clientID}`;
     try {
+      setLoading(true);
       const response = await fetch(url);
       const data = await response.json();
-      console.log(data);
+      setPhotos(data);
+      setLoading(false);
     } catch (error) {
       console.log(error);
+      setLoading(false);
     }
   };
 
   useEffect(() => {
     fetchImages();
-  });
+  }, []);
 
-  return <h2>stock photos starter</h2>;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("hello");
+  };
+
+  return (
+    <main>
+      <section className="search">
+        <form className="search-form" onSubmit={handleSubmit}>
+          <input type="text" placeholder="search" className="form-input" />
+          <button type="submit" className="submit-btn">
+            <FaSearch />
+          </button>
+        </form>
+      </section>
+      <section className="photos">
+        <div className="photos-center">
+          {photos.map((photo) => {
+            return <Photo key={photo.id} {...photo} />;
+          })}
+        </div>
+      </section>
+      {loading && <h2 className="loading">Loading...</h2>}
+    </main>
+  );
 }
 
 export default App;
